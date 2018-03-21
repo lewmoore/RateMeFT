97c3f94a14779a3739db23541d35c248b1f83132
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-21 15:23:08 +0000

    install Mocha & Chai deps. Split files in to src and test directories. Test for working landing page with Welcome text

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => res.send('Welcome!'))
+
+app.listen(3000, () => console.log('Your on localhost 3000'))
+
+module.exports = app
---
+++ test/test.js
@@ -0,0 +1,25 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../src/app');
+
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Testing landing page', function() {
+  it('shouldnt error', function() {
+    chai.request(app)
+    .get('/')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+    });
+  });
+
+  it('should display welcome text', function() {
+    chai.request(app)
+    .get('/')
+    .end(function(err,res) {
+      expect(res.text).to.equal('Welcome!');
+    });
+  });
+});
