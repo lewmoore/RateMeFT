f0c3b35e229389d206e4352e852321400ef0e087
Lewis Moore <lewis.moore2707@gmail.com>
2018-03-24 15:01:58 +0000

    Add beginnings on MongoDb database to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express()
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017/ratings')
+var ratingSchema = new mongoose.Schema({
+  dropdown: String
+})
+var Rating = mongoose.model('Rating', ratingSchema)
 app.set("view engine", "pug");
 app.set("views", "views")
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
@@ -13,9 +21,19 @@ app.get('/', function(req, res) {
 
  app.post('/success', function(req, res){
   this.rating = req.body.dropdown
-   res.render("success")
+  console.log(req.body)
+  var myRating = new Rating(req.body);
+    myRating.save()
+    .then(item => {
+      res.render("success")
+    })
+    .catch(err => {
+      res.status(400).send("Not Saved")
+    });
  })
 
 app.listen(8081, () => console.log('Your on localhost 8081'))
+
+
 
 module.exports = app
